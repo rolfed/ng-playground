adfea822fda7cab2dbabff296eac21950fdb76b7
Danniel Rolfe <talkto@dannielrolfe.com>
2022-12-25 12:19:05 -0800

    Scaffold jobs and view-title

---
+++ src/app/shared/view-title/view-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-title',
+  templateUrl: './view-title.component.html',
+  styleUrls: ['./view-title.component.scss']
+})
+export class ViewTitleComponent {
+
+}
---
+++ src/app/shared/view-title/view-title.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewTitleComponent } from './view-title.component';
+
+
+
+@NgModule({
+  declarations: [
+    ViewTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewTitleModule { }
