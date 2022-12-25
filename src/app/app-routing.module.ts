4485c1914c437e05988f5a945f177741da130203
Danniel Rolfe <talkto@dannielrolfe.com>
2022-12-25 12:30:23 -0800

    Add user and jobs routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'jobs'
+  },
+  {
+    path: 'jobs', loadChildren: () => import('./views/jobs/jobs.module').then(m => m.JobsModule),
+  },
+  {
+    path: 'user/:id', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
